cb7143e88e533a44deccd05acd7e52e6e07fe218
Andrey Ligiza <andrey.ligiza@mail.ru>
2024-11-10 21:37:04 +0300

    feat: добавление уровня роутинга player/partner

---
+++ src/app/game/[id]/[user]/pl-to-pr/page.tsx
@@ -0,0 +1,17 @@
+'use client'
+import { useParams } from "next/navigation";
+
+export default function Home() {
+    // if currentRole === 'player' грузим зеленые данные 
+    // if currentRole === 'partner' грузим синие данные 
+    const params = useParams<{ id: string, user: string }>();
+    console.log(params);
+    
+    return (
+        <div>
+            <main>
+                result partner
+            </main>
+        </div>
+    );
+}
--- src/app/game/[id]/start/page.tsx
+++ src/app/game/[id]/start/page.tsx
@@ -1,50 +1,9 @@
-// import Link from "next/link";
-
-// type Product = {
-//     id: number;
-//     name: string;
-//     price: number;
-//     isAvailable: boolean;
-// };
-
-// async function getProducts() {
-//     try {
-//         const res = await fetch(`https://jsonplaceholder.typicode.com/users`);
-//         const products = await res.json();
-
-//         return products;
-//     } catch (error) {
-//         console.error(error);
-//     }
-// }
-
-// export default async function Page({ params }: { params: { id: string } }) {
-//     const products = await getProducts();
-//     return (
-//         <div>
-//             <Link href={`/game/${params.id}/result-player`}>Back</Link>
-//             {products.map((product: Product) => (
-//                 <div key={product.id}>
-//                     {product.name} {params.id}
-//                 </div>
-//             ))}
-//         </div>
-//     );
-// }
-
 import RegistrationForms from "@/shared/components/RegistrationForms/RegistrationForms";
-import { StartInfo } from "@/shared/components/StartInfo/StartInfo";
 
 export default function StartPage() {
-    
-    const currentUser = "player";
-    if (currentUser === "player") {
-        return (
-            <div className="container">
-                <RegistrationForms />
-            </div>
-        );
-    }
-
-    return <div className="container"> </div>;
+    return (
+        <div className="container">
+            <RegistrationForms />
+        </div>
+    );
 }
